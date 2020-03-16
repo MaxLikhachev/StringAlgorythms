715a3841d93a8f51d28225bb91dc8447ed47536d
maximlihachev <maxim.lihachev@vistar.su>
2020-03-16 11:59:06 +0300

    Added Border Array Algos

--- algorythm/PrefixBorderArray.py
+++ algorythm/PrefixBorderArray.py
@@ -1,27 +1,27 @@
 import re
 
-class PrefixBorderArray:
+class PrefixborderPrefixs:
 
 
     def __init__(self, string):
         self.string = string
-        self.borderArray = [0 for i in range(0,len(self.string))]
+        self.borderPrefixs = [0 for i in range(0,len(self.string))]
 
         self.execute()
 
 
     def execute(self):
         n = len(self.string)
-        self.borderArray[0] = 0
+        self.borderPrefixs[0] = 0
         for i in range(1,n):
-            borderPrefixRight = self.borderArray[i - 1]
+            borderPrefixRight = self.borderPrefixs[i - 1]
 
             while borderPrefixRight and (self.string[i] != self.string[borderPrefixRight]) :
-                borderPrefixRight = self.borderArray[borderPrefixRight - 1]
+                borderPrefixRight = self.borderPrefixs[borderPrefixRight - 1]
             
-            if self.string[i] == self.string[borderPrefixRight]: self.borderArray[i] = borderPrefixRight + 1
-            else: self.borderArray[i] = 0
+            if self.string[i] == self.string[borderPrefixRight]: self.borderPrefixs[i] = borderPrefixRight + 1
+            else: self.borderPrefixs[i] = 0
         
 
     def __str__(self):
-        return "Border array is: " + re.sub(r'\[|\]', '', str(self.borderArray)) 
+        return "Border array is: " + re.sub(r'\[|\]', '', str(self.borderPrefixs)) 
--- algorythm/SuffixBorderArray.py
+++ algorythm/SuffixBorderArray.py
@@ -1,28 +1,28 @@
 import re
 
-class SuffixBorderArray:
+class SuffixborderSuffixs:
 
 
     def __init__(self, string):
         self.string = string
-        self.borderArray = [0 for i in range(0,len(self.string))]
+        self.borderSuffixs = [0 for i in range(0,len(self.string))]
 
         self.execute()
 
 
     def execute(self):
         n = len(self.string) 
-        self.borderArray[n - 1] = 0
+        self.borderSuffixs[n - 1] = 0
         for i in range(n-2,0):
-            borderSuffixLeft = self.borderArray[i + 1]
+            borderSuffixLeft = self.borderSuffixs[i + 1]
             
             while borderSuffixLeft and (self.string[i] != self.string[n - borderSuffixLeft - 1]): 
-                borderSuffixLeft = self.borderArray[n - borderSuffixLeft]
+                borderSuffixLeft = self.borderSuffixs[n - borderSuffixLeft]
 
             if self.string[i] == self.string[n - borderSuffixLeft - 1]:  
-                self.borderArray[i] = borderSuffixLeft + 1
+                self.borderSuffixs[i] = borderSuffixLeft + 1
             else:
-                self.borderArray[i] = 0
+                self.borderSuffixs[i] = 0
 
     def __str__(self):
-        return "Border array is: " + re.sub(r'\[|\]', '', str(self.borderArray)) 
+        return "Border array is: " + re.sub(r'\[|\]', '', str(self.borderSuffixs)) 
